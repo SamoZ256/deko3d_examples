da01103147f749d405371d3bfc1d0cc479361190
Samuliak <samuliak77@gmail.com>
2024-10-17 15:38:46 +0200

    reorganize folders

--- src/examples/triangle/main.cpp
+++ src/examples/triangle/main.cpp
@@ -1,30 +1,19 @@
-// Include the most common headers from the C standard library
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// Include the main libnx system header, for Switch development
 #include <switch.h>
+#include <deko3d.hpp>
 
 // Main program entrypoint
 int main(int argc, char* argv[])
 {
-    // This example uses a text console, as a simple way to output text to the screen.
-    // If you want to write a software-rendered graphics application,
-    //   take a look at the graphics/simplegfx example, which uses the libnx Framebuffer API instead.
-    // If on the other hand you want to write an OpenGL based application,
-    //   take a look at the graphics/opengl set of examples, which uses EGL instead.
-    consoleInit(NULL);
-
     // Configure our supported input layout: a single player with standard controller styles
     padConfigureInput(1, HidNpadStyleSet_NpadStandard);
 
     // Initialize the default gamepad (which reads handheld mode inputs as well as the first connected controller)
     PadState pad;
     padInitializeDefault(&pad);
-
-    // Other initialization goes here. As a demonstration, we print hello world.
-    printf("Hello World!\n");
 
     // Main loop
     while (appletMainLoop())
@@ -38,14 +27,7 @@ int main(int argc, char* argv[])
 
         if (kDown & HidNpadButton_Plus)
             break; // break in order to return to hbmenu
-
-        // Your code goes here
-
-        // Update the console, sending a new frame to the display
-        consoleUpdate(NULL);
     }
 
-    // Deinitialize and clean up resources used by the console (important!)
-    consoleExit(NULL);
     return 0;
 }
